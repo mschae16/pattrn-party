6f225a62b0341f068824ca0e61746f84ff04446a
Margo Schaedel <margoschaedel@gmail.com>
2017-10-17 17:38:36 -0600

    Refactor javascript

--- scripts.js
+++ scripts.js
@@ -1,26 +1,20 @@
 
 const toggleTabs = (e) => {
-  const currentlyActive = $('li.tab').hasClass('active');
-
-  const targetActive = $(e.target).closest('.tab').hasClass('active');
+ const targetActive = $(e.target).closest('.tab').hasClass('active');
 
   if (targetActive) {
     $('.active').find('.toggle').text('+');
     $('.active').removeClass('active');
-    return
-  }
-
-  if (currentlyActive) {
+  } else {
     $('.active').find('.toggle').text('+');
     $('.active').removeClass('active');
-  }
+    $(e.target).closest('.tab').addClass('active');
 
-  $(e.target).closest('.tab').addClass('active');
-
-  if ($( window ).width() < 680) {
-    $(e.target).text('-');
+    $(window).width() < 680 ?
+      $(e.target).closest('.tab').find('.toggle').text('-')
+      :
+      null
   }
 }
-
 
 $('.tab-header').on('click', (e) => toggleTabs(e));
